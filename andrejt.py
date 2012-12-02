187ef6782cdc08b3d9fd7a387e35ae28eeb9e783
Andrej T <andrejtrajchevski@gmail.com>
2012-12-02 23:38:59 +0100

    reloader

--- andrejt.py
+++ andrejt.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template
+from subprocess import call
 
 app = Flask(__name__)
-app.debug = True
+# app.debug = True
 
 @app.route('/')
 def index():
 	return render_template("index.html")
 
+@app.route('/update')
+def update():
+	call(['git', 'pull'])
+	return "Success."
+
 if __name__ == "__main__":
-	app.run("0.0.0.0", port=80)
+	app.run("0.0.0.0", port=80, use_reloader=True)
