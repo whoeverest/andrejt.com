203d6d96cce4b3ee51db3f05086f81ea3de166b3
Andrej T <andrejtrajchevski@gmail.com>
2012-12-03 14:37:04 +0100

    ie revert

--- andrejt.py
+++ andrejt.py
@@ -13,9 +13,5 @@ def update():
 	call(['git', 'pull'])
 	return "Success."
 
-@app.route('/dali_mi_raboti_sajtot_na_internet_explorer')
-def ie():
-	return "Ne."
-
 if __name__ == "__main__":
 	app.run("0.0.0.0", port=80, use_reloader=True)
